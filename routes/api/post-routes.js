5551bbbfecc1d0d41b7315eabe19ce9dfdfba0f2
Taylor Nyquist <tnyquist@gmail.com>
2020-08-30 11:00:32 -0500

    finished comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all posts
@@ -7,14 +7,23 @@ router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         // Query configuration
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
             'title',
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-        order: [['created_at', 'DESC']],
         include: [
+            // include the Comment and User models here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -43,6 +52,14 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
---
+++ seed.js
@@ -0,0 +1,66 @@
+// comments
+
+{
+    "comment_text": "testing 123",
+    "user_id": 1,
+    "post_id": 3
+  }
+
+  // posts
+
+  [
+    {
+      "id": 4,
+      "post_url": "https://test.com/",
+      "title": "test title 2",
+      "created_at": "2020-08-29T04:55:24.000Z",
+      "vote_count": 0,
+      "user": {
+        "username": "bob"
+      }
+    },
+    {
+      "id": 3,
+      "post_url": "https://test.com/",
+      "title": "test title",
+      "created_at": "2020-08-29T04:55:15.000Z",
+      "vote_count": 1,
+      "user": {
+        "username": "bob"
+      }
+    },
+    {
+      "id": 2,
+      "post_url": "https://portfolio.com/",
+      "title": "Portfolio reaches 2 million subscribers",
+      "created_at": "2020-08-29T04:26:37.000Z",
+      "vote_count": 1,
+      "user": {
+        "username": "bob"
+      }
+    }
+  ]
+
+  // users
+
+  [
+    {
+      "id": 1,
+      "username": "bob",
+      "email": "bob@gmail.com"
+    }
+  ]
+
+  // upvotes
+
+  {
+	"user_id": 1,
+    "post_id": 4
+}
+
+// login post
+
+{
+    "email": "lernantino@gmail.com",
+		"password": "password1234"
+}
